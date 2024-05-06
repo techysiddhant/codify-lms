09a7eb2b12e904ab36320d45957ee0614a2be366
techysiddhant <sidjain8000@gmail.com>
2024-05-06 18:37:32 +0530

    update the logo

--- lms/app/(portal)/_components/logo.tsx
+++ lms/app/(portal)/_components/logo.tsx
@@ -1,5 +1,14 @@
 import Image from "next/image";
+import Link from "next/link";
+import { Pacifico } from "next/font/google";
+const pacifico = Pacifico({ subsets: ["cyrillic"],weight:["400"] });
 
 export const Logo = () => {
-  return <Image height={130} width={130} alt="logo" src="/logo.svg" />;
+  // return <Image height={130} width={130} alt="logo" src="/logo.svg" />;
+  return(
+    <div className="">
+      <h1 className={`${pacifico.className} uppercase text-2xl font-bold text-primary tracking-wide `}>Codify</h1>
+      <h2 className="text-sm italic">by <Link href='/' className="underline font-medium italic">Siddhant Jain</Link>  </h2>
+    </div>
+  )
 };
--- lms/app/(portal)/_components/sidebar.tsx
+++ lms/app/(portal)/_components/sidebar.tsx
@@ -4,10 +4,10 @@ import { SidebarRoutes } from "./sidebar-routes";
 export const Sidebar = () => {
 	return (
 		<div className="h-full border-r flex flex-col overflow-y-auto bg-primary-foreground  shadow-sm">
-			<div className="p-6">
+			<div className="p-4">
 				<Logo />
 			</div>
-			<div className="flex flex-col w-full mt-1">
+			<div className="flex flex-col w-full -mt-1">
 				<SidebarRoutes />
 			</div>
 		</div>
