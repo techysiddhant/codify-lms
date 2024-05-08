add99c0c6aef72c507984c21ce46c7ccaf65cdc6
techysiddhant <sidjain8000@gmail.com>
2024-05-08 17:03:19 +0530

    fix default theme

--- lms/app/layout.tsx
+++ lms/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
 				<NextAuthProvider>
 					<ThemeProvider
 						attribute="class"
-						defaultTheme="system"
+						defaultTheme="light"
 						enableSystem
 						disableTransitionOnChange
 					>
